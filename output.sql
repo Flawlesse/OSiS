1346924d665a0b15d7d5c3b6fb6a59aa0e4ae1aa
Alexey Lavrenovich <lavrenovichae@gmail.com>
2022-04-04 21:15:26 +0300

    labs added

---
+++ output.sql
@@ -0,0 +1,2 @@
+INSERT INTO Customers(first_name, last_name, phone_number, money) VALUES(Misha, Kireev, +20157784448, 201.15);
+INSERT INTO Customers(first_name, last_name, phone_number, money) VALUES(Alexey, Sachanov, +375297894561, 1888.13);
